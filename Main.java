07f90e7c3e12ffe73a959c75cf747ca6ec116cd5
Chiny <ChinyaU@yandex.ru>
2024-10-30 21:15:35 +0300

    created class Main

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Hello");
+    public static void main(String[] args) {
+        isChetNum();
+    }
+
+    public static void isChetNum() {
+        System.out.println("Введите число: ");
+        int num = new Scanner(System.in).nextInt();
+        if (num % 2 == 0) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
     }
 }

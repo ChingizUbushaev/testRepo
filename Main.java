2eb6205afcb25ef975421a864280d611af6f6e58
Chiny <ChinyaU@yandex.ru>
2024-11-03 16:48:38 +0300

    Added a class for books

--- Main.java
+++ Main.java
@@ -1,17 +1,46 @@
 import java.util.Scanner;
 
 public class Main {
+    static String booksInfo = "";
+
     public static void main(String[] args) {
-        isChetNum();
+        while (true) {
+            String name = inputBookName();
+            int pageCount = inputPageCount();
+            int priceBook = inputPraceBook();
+            addBook(name, pageCount, priceBook);
+            printInfo();
+        }
     }
 
-    public static void isChetNum() {
-        System.out.println("Введите число: ");
-        int num = new Scanner(System.in).nextInt();
-        if (num % 2 == 0) {
-            System.out.println("True");
-        } else {
-            System.out.println("False");
-        }
+    public static int inputPageCount() {
+        System.out.println("Input count pages:");
+        return new Scanner(System.in).nextInt();
+    }
+
+    public static int inputPraceBook() {
+        System.out.println("Input count pages:");
+        return new Scanner(System.in).nextInt();
+    }
+
+    public static String inputBookName() {
+        System.out.println("Input name of book:");
+        return new Scanner(System.in).nextLine();
+    }
+
+    public static void addBook(String bookName) {
+        addBook(bookName, 0, 0);
+    }
+
+    public static void addBook(String bookName, int pageCount) {
+        addBook(bookName, 0, 0);
+    }
+
+    public static void addBook(String bookName, int pageCount, int praceBook) {
+        booksInfo += bookName + " - " + (pageCount > 0 ? pageCount : "N/A") + " стр." + " " + (praceBook > 0 ? praceBook : "N/A") + " руб." + "\n";
+    }
+
+    public static void printInfo() {
+        System.out.println(booksInfo);
     }
 }
